109b016ce2167801ce4ca8b789b8c5f309b3ba75
Dara_J <faithlohla@gmail.com>
2022-01-09 17:34:56 -0500

    assert elements are visible before click or type

--- cypress/integration/deel/Deel app.js
+++ cypress/integration/deel/Deel app.js
@@ -13,12 +13,22 @@ describe('Create Fixed Contract', () => {
 
     it('Create Fixed Contract', () => 
     {
-        cy.get(":nth-child(2) > .anchor > .sidebar-link > .flex > .sidebar-option-p").click() //click on create a contract
-        cy.get(':nth-child(1) > .anchor > .box').click() //click on fixed contract
-        cy.get(':nth-child(1) > :nth-child(1) > .deel-ui__input-component__wrapper > .deel-ui__input-component > .deel-ui__input-component__input').type(fixeddata.contract_name)
-        //cy.get("#root > div > div > div:nth-child(4) > div > form > div.box.mb-10.undefined > div > div.deel-ui__stack.deel-ui__stack_vertical.deel-ui__stack_with-spacing > div > div > div > div > div > div.deel-ui__select__value-container.css-1hwfws3")
-        //.select('Canada')
-        //.should('have.value','Canada')
-        cy.get("[name='jobTitle']").type(fixeddata.job_title) //Enter job title
+        cy.get(":nth-child(2) > .anchor > .sidebar-link > .flex > .sidebar-option-p").should("be.visible")
+        .click() //click on create a contract
+
+        cy.get(':nth-child(1) > .anchor > .box').should("be.visible")
+        .click() //click on fixed contract
+
+        cy.get(':nth-child(1) > :nth-child(1) > .deel-ui__input-component__wrapper > .deel-ui__input-component > .deel-ui__input-component__input').should("be.visible")
+        .type(fixeddata.contract_name)
+
+        cy.get(':nth-child(1) > .deel-ui__stack_vertical > .input-container > .flex > .deel-ui__select > .deel-ui__select__input-container > .deel-ui__select__control > .deel-ui__select__value-container').should("be.visible")
+        .type(fixeddata.tax_residence)
+        
+        cy.get(':nth-child(3) > :nth-child(1) > .deel-ui__input-component__wrapper > .deel-ui__input-component > .deel-ui__input-component__input').should("be.visible")
+        .type(fixeddata.job_title) //Enter job title
+        
+        cy.get('.deel-ui__textarea-component__textarea').should("be.visible")
+        .type(fixeddata.scope_of_work) //Enter scope of work
     })
 })
